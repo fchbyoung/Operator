2c725ecfb92e70a3771728eee1e9cb33269a5a1d
Long <fchbyoung1010@gmail.com>
2019-06-24 08:39:00 +0700

    Operator

---
+++ src/main/java/Operator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Operator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        float width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        float height = scanner.nextFloat();
+
+        float area = width * height;
+        System.out.println("area = " + area);
+    }
+
+
+}
